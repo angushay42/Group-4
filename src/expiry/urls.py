29071f25776b7773f5d656802272bb9db952c524
16CRichardson <16.c.richardson@kings.lincs.sch>
2025-11-13 23:43:16 +0000

    Tailwind has been installed

--- src/expiry/urls.py
+++ src/expiry/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login, name="login")
+    path("<int:question>/", views.login, name="login")
 ]
--- src/expiry/views.py
+++ src/expiry/views.py
@@ -1,15 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Question
 
 def login(request):
     template = loader.get_template("expiry/login.html")
-    context = {}
-    return template.render(context, request)
-    context = {}
+    context = {"Login" : template}
     return render(request, "expiry/login.html", context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
