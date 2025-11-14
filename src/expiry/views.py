3e65548282d7d0f7075c28ec63e7352b4ff1fc0d
Angus Hay <angushay42@gmail.com>
2025-11-14 11:19:58 +0000

    BUG: email and password not passed into parameters for login page. trying to decipher Django docs.

--- src/expiry/views.py
+++ src/expiry/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user
 
 from .models import Question
 
 def login(request):
+    if 'username' in request.POST and 'password' in request.POST:
+        print("yippee!")
+        pass    # do stuff with details
+
     template = loader.get_template("expiry/login.html")
     context = {"Login" : template}
+
     return render(request, "expiry/login.html", context)
+
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
