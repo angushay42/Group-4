34d8f71a7c64846a8933e69f3f6361eb6e540153
16CRichardson <16.c.richardson@kings.lincs.sch>
2025-11-18 17:34:49 +0000

    created a registartion form that now needs styling to be abe to implement

--- src/expiry/views.py
+++ src/expiry/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import get_user
-
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.core.validators import EmailValidator, validate_email
 from django.core.handlers.wsgi import WSGIRequest
@@ -18,12 +17,16 @@ def startup(request):
     logger.debug("startup page viewed")
     return render(request, 'expiry/startup.html')
 
+def logout_view(request):
+    logout(request)
+    return render(request, "expiry/login.html")
+
 def login_view(request: WSGIRequest):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect("expiry:dashboard")
+            return redirect("expiry/dashboard.html")
     else:
         form = AuthenticationForm()
     return render(request, "expiry/login.html", {"form" : form})
@@ -38,7 +41,11 @@ def signup_view(request):
         form = UserCreationForm()
     return render(request, 'expiry/signup.html', {"form" : form})
 
+
 def dashboard(request):
-    return render(request, 'expiry/dashboard.html')
+    if not request.user.is_authenticated:    #limits access when not logged in
+        return render(request, "expiry/login.html")
+    else:
+        return render(request, 'expiry/dashboard.html')
 
 
