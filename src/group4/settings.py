1291799804dbacfd45595d786e662d90b51ed2d1
CRichardsonGit <charliejoe2005@gmail.com>
2025-12-16 17:20:58 +0000

    Made it so class based darkmode actually works based on the toggle

--- src/group4/settings.py
+++ src/group4/settings.py
@@ -75,13 +75,14 @@ ROOT_URLCONF = 'group4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'expiry.context_processors.user_settings',
             ],
         },
     },
